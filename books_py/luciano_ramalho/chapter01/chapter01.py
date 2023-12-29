1ab63110ad33c79aaff28432aee235f6bfe9bc41
calebeLAR <ca6ele@gmail.com>
2023-12-29 00:14:22 -0300

    cap01

---
+++ books_py/luciano_ramalho/chapter01/chapter01.py
@@ -0,0 +1,15 @@
+class Chapter01:
+    topics = (
+        "Capítulo 1 ■ Modelo de dados do Python",
+        "Um baralho pythônico",
+        "Como os métodos especiais são usados",
+        "Emulando tipos numéricos",
+        "Representação em string",
+        "Operadores aritméticos",
+        "Valor booleano de um tipo definido pelo usurio",
+        "Visão geral dos métodos especiais",
+        "Por que len não é um método?",
+        "Resumo do captulo",
+        "Leituras complementares",
+    )
+    pass
---
+++ books_py/luciano_ramalho/exemples/exemple11.py
@@ -0,0 +1,40 @@
+# Exemplo 1.1 - Um baralho como uma sequência de cartas
+# executar no Ipython
+
+import collections
+from random import choice
+
+Card = collections.namedtuple("Card", ["rank", "suit"])
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list("JQKA")
+    suits = "spades diamonds clubs hearts".split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+if __name__ == "__main__":
+    beer_card = Card("7", "diamonds")
+    deck = FrenchDeck()
+    print(beer_card)
+
+    print(choice(deck))
+
+    for card in sorted(deck, key=spades_high):  # doctest: +ELLIPSIS
+        print(card)
+
+    print(deck[12::13])
