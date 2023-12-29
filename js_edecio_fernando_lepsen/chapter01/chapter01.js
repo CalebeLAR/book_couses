92c5becb4ad3556eed59a9b8ab7c5216799a2c95
calebeLAR <ca6ele@gmail.com>
2023-12-29 19:34:06 -0300

    chapter01

---
+++ js_edecio_fernando_lepsen/chapter01/chapter01.js
@@ -0,0 +1,13 @@
+class Chapter01 {
+    topics = [
+        'capítulo 1 Introdução',
+        '1.1 Lógica de programação',
+        '1.1.1 Compreender o que é pedido',
+        '1.1.2 Realizar deduções na construção do programa',
+        '1.1.3 Enumerar as etapas a serem realizadas',
+        '1.1.4 Analisar outras possibilidades de solução',
+        '1.1.5 Ensinar ao computador uma solução',
+        '1.1.6 Pensar em todos os detalhes',
+        '1.2 Entrada, processamento e saída',
+    ];
+}
