31e6c41786e1d661061cdae2ffecf360e4b1af38
calebeLAR <ca6ele@gmail.com>
2024-01-16 14:46:26 -0300

    chapter01

---
+++ py_eric_matthes/chapter01/exes.py
@@ -0,0 +1,20 @@
+# flake8: noqa 
+
+# 1.1 – python.org: Explore a página inicial de Python (http://python.org/) para
+# encontrar tópicos pelos quais você se interesse. À medida que tiver mais
+# familiaridade com Python, diferentes partes do site serão mais úteis a você.
+
+# 1.2 – Erros de digitação em Hello World: Abra o arquivo hello_world.py que
+# você acabou de criar. Introduza um erro de digitação em algum ponto da linha
+# e execute o programa novamente. Você é capaz de inserir um erro de
+# digitação que gere um erro? Consegue entender a mensagem de erro? É
+# capaz de inserir um erro de digitação que não produza um erro? Por que você
+# acha que isso não gerou um erro?
+
+# 1.3 – Habilidades infinitas: Se tivesse infinitas habilidades de programação, o
+# que você desenvolveria? Você está prestes a aprender a programar. Se tiver um
+# objetivo final em mente, você terá um uso imediato para suas novas habilidades;
+# agora é um ótimo momento para esboçar descrições do que você gostaria de
+# criar. É um bom hábito manter um caderno com “ideias” que você possa
+# consultar sempre que quiser iniciar um novo projeto. Reserve alguns minutos para
+# descrever três programas que você gostaria de criar.
