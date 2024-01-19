364161d986cf972d3a4951647d810faf925a9d05
calebeLAR <ca6ele@gmail.com>
2024-01-19 00:02:30 -0300

    chapter02

---
+++ py_eric_matthes/chapter02/exercises/number_eight.py
@@ -0,0 +1,6 @@
+# 2.9 – Número favorito: Armazene seu número favorito em uma variável. Em
+# seguida, usando essa variável, crie uma mensagem que revele o seu número
+# favorito. Exiba essa mensagem.
+favorite_number = 1
+message = "meu numero favorito é: " + str(favorite_number)
+print(message)
